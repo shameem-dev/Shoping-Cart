2438b26c13105c5ec33304703cf26409ff879048
Shameem <shameemalit96@gmail.com>
2024-07-12 18:18:30 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './views/**/*.hbs',
+    './public/**/*.js',
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
